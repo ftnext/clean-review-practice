383782ee06c785d6cc6c656bc275ace4799f3e7c
ftnext <takuyafjp+develop@gmail.com>
2021-06-09 14:16:00 +0900

    [refactor] Refactor with setUp to prevent repetition

--- reviewlogic/tests/gateways/test_proposal_gateway.py
+++ reviewlogic/tests/gateways/test_proposal_gateway.py
@@ -7,6 +7,10 @@ from reviewlogic.gateways import proposal_gateway
 
 
 class ProposalGatewayTestCase(TestCase):
+    def setUp(self):
+        self.mock_driver = MagicMock(proposal_driver.ProposalDriver)
+        self.gateway = proposal_gateway.ProposalGateway(self.mock_driver)
+
     @patch("reviewlogic.domains.proposal.Proposal.from_entity")
     def test_list(self, from_entity):
         entity1 = proposal_driver.ProposalEntity(
@@ -15,16 +19,14 @@ class ProposalGatewayTestCase(TestCase):
         entity2 = proposal_driver.ProposalEntity(
             id=2, title="Title 2", description="Description 2"
         )
-        mock_driver = MagicMock(proposal_driver.ProposalDriver)
-        mock_driver.find_all.return_value = [entity1, entity2]
+        self.mock_driver.find_all.return_value = [entity1, entity2]
         proposal1 = MagicMock(spec=proposal.Proposal)
         proposal2 = MagicMock(spec=proposal.Proposal)
         from_entity.side_effect = (proposal1, proposal2)
         expected = proposal.Proposals([proposal1, proposal2])
 
-        gateway = proposal_gateway.ProposalGateway(mock_driver)
-        actual = gateway.list()
+        actual = self.gateway.list()
 
         self.assertEqual(actual, expected)
-        mock_driver.find_all.assert_called_once_with()
+        self.mock_driver.find_all.assert_called_once_with()
         from_entity.assert_has_calls([call(entity1), call(entity2)])
