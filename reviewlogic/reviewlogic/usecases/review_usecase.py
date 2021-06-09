a2fffcc7051440a4fc619b781848f7779df6eb28
ftnext <takuyafjp+develop@gmail.com>
2021-06-09 16:13:15 +0900

    [feat] Implement ReviewUseCase.save()

--- reviewlogic/reviewlogic/usecases/review_usecase.py
+++ reviewlogic/reviewlogic/usecases/review_usecase.py
@@ -1,6 +1,10 @@
+from reviewlogic.domains import review
+
+
 class ReviewUseCase:
     def __init__(self, port) -> None:
         self.port = port
 
     def save(self, user_id, proposal_id, score, comment):
-        raise NotImplementedError
+        review_object = review.Review(user_id, proposal_id, score, comment)
+        return self.port.save(review_object)
