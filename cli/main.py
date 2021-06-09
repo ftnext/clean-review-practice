0e43ad42f46536ca29c4bc430ba3370d3431c8d5
ftnext <takuyafjp+develop@gmail.com>
2021-06-09 15:34:28 +0900

    [feat] User can select each proposal with minimum CLI app

--- cli/main.py
+++ cli/main.py
@@ -1,7 +1,7 @@
 from reviewlogic.drivers import proposal_driver
 from reviewlogic.gateways import proposal_gateway
 from reviewlogic.usecases import proposal_usecase
-
+from reviewlogic.value_objects import ProposalId
 
 if __name__ == "__main__":
     driver = proposal_driver.InMemoryProposalDriver()
@@ -16,5 +16,31 @@ if __name__ == "__main__":
     for p in proposals:
         print(f"- {p.id} {p.title}")
     print("")
-    
-    input("Select id to review target [ex. 108]: ")
+
+    while True:
+        entered = input(
+            "Select id to review target [ex. 108] (Exit [Press q]): "
+        )
+        stripped = entered.strip()
+        if not stripped:
+            continue
+        if stripped.lower() == "q":
+            break
+
+        try:
+            entered_id = int(stripped)
+        except ValueError:
+            print("Error: Please input as an integer")
+            continue
+
+        proposal_id = ProposalId(entered_id)
+        try:
+            proposal = usecase.find_by(proposal_id)
+        except ValueError as ex:
+            print(f"Error: {ex}")
+            continue
+
+        print(f"{proposal.title} (Id={proposal.id})")
+        print("")
+        print(proposal.description)
+        print("")
