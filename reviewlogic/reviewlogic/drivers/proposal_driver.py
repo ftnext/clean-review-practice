feceeb4ded3eb8a1dbc5c945a6c341302ba2ab58
ftnext <takuyafjp+develop@gmail.com>
2021-06-09 11:38:16 +0900

    [feat] Add in-memory proposal driver

--- reviewlogic/reviewlogic/drivers/proposal_driver.py
+++ reviewlogic/reviewlogic/drivers/proposal_driver.py
@@ -11,3 +11,13 @@ class ProposalDriver(metaclass=abc.ABCMeta):
     @abc.abstractmethod
     def find_all(self):
         raise NotImplementedError
+
+
+class InMemoryProposalDriver(ProposalDriver):
+    proposal_entities = [
+        ProposalEntity(title="Pythonのspamについて", description="spam ham egg"),
+        ProposalEntity(title="熱のこもったプロポーザル", description="とにかく熱い本文"),
+    ]
+
+    def find_all(self):
+        return self.proposal_entities
