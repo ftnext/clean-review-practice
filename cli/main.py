606d6ab0dc178d5cbcb4b100283c649d49f22d81
ftnext <takuyafjp+develop@gmail.com>
2021-06-09 11:49:24 +0900

    [feat] Minimum CLI to list up proposals

---
+++ cli/main.py
@@ -0,0 +1,17 @@
+from reviewlogic.drivers import proposal_driver
+from reviewlogic.gateways import proposal_gateway
+from reviewlogic.usecases import proposal_usecase
+
+
+if __name__ == "__main__":
+    driver = proposal_driver.InMemoryProposalDriver()
+    port = proposal_gateway.ProposalGateway(driver)
+    usecase = proposal_usecase.ProposalUseCase(port)
+
+    input("Welcome to the review app [Press Enter]")
+    print("")
+
+    proposals = usecase.list()
+    print("Proposals\n")
+    for p in proposals:
+        print(f"- {p.title}")
