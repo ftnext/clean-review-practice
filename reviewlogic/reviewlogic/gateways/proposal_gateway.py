a9e9f8de348c8f4b748758533c115ee18795ae50
ftnext <takuyafjp+develop@gmail.com>
2021-06-09 11:10:38 +0900

    [feat] Implement ProposalGateway.list()

--- reviewlogic/reviewlogic/gateways/proposal_gateway.py
+++ reviewlogic/reviewlogic/gateways/proposal_gateway.py
@@ -1,6 +1,14 @@
+from reviewlogic.domains import proposal
+
+
 class ProposalGateway:
     def __init__(self, driver):
         self.driver = driver
 
     def list(self):
-        raise NotImplementedError
+        entities = self.driver.find_all()
+        proposals = [
+            proposal.Proposal(entity["title"], entity["description"])
+            for entity in entities
+        ]
+        return proposal.Proposals(proposals)
