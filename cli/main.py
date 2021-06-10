af2b0454a26081a3692262e535f0969b5231c711
ftnext <takuyafjp+develop@gmail.com>
2021-06-10 12:37:42 +0900

    [feat] User can save review from CLI

--- cli/main.py
+++ cli/main.py
@@ -1,14 +1,24 @@
 from reviewlogic.domains import proposal as p
+from reviewlogic.domains import review as r
 from reviewlogic.drivers import proposal_driver as pd
+from reviewlogic.drivers import review_driver as rd
 from reviewlogic.gateways import proposal_gateway as pg
+from reviewlogic.gateways import review_gateway as rg
 from reviewlogic.usecases import proposal_usecase as pu
-from reviewlogic.value_objects import ProposalId
+from reviewlogic.usecases import review_usecase as ru
+from reviewlogic.value_objects import ProposalId, UserId
 
 
 def configure_proposal_logic() -> pu.ProposalUseCase:
     proposal_driver = pd.InMemoryProposalDriver()
     proposal_port = pg.ProposalGateway(proposal_driver)
     return pu.ProposalUseCase(proposal_port)
+
+
+def configure_review_logic() -> ru.ReviewUseCase:
+    review_driver = rd.InMemoryReviewDriver()
+    review_port = rg.ReviewGateway(review_driver)
+    return ru.ReviewUseCase(review_port)
 
 
 def show_proposals_list(proposals: p.Proposals) -> None:
@@ -25,11 +35,26 @@ def show_proposal_detail(proposal: p.Proposal) -> None:
     print("")
 
 
+def enter_review_score() -> r.ScoreEnum:
+    while True:
+        entered = input("Input score [Yes / Maybe / No]: ")
+        try:
+            score = r.ScoreEnum(entered.strip().title())
+        except ValueError:
+            print(f"Error: {entered} is not valid as score")
+            continue
+        return score
+
+
 if __name__ == "__main__":
     proposal_usecase = configure_proposal_logic()
+    review_usecase = configure_review_logic()
 
     input("Welcome to the review app [Press Enter]")
     print("")
+
+    # Dummy login
+    USER_ID = UserId(101)
 
     proposals = proposal_usecase.list()
     show_proposals_list(proposals)
@@ -58,3 +83,12 @@ if __name__ == "__main__":
             continue
 
         show_proposal_detail(proposal)
+
+        entered = input("Review this proposal? [y/N] ")
+        if entered.strip().lower() != "y":
+            continue
+
+        score = enter_review_score()
+        comment = input("Input comment: ")
+
+        review_usecase.save(USER_ID, proposal_id, score, comment)
