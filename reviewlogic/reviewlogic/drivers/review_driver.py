9bc71581f0ee664caf7ed099d72cc46ebb6b842d
ftnext <takuyafjp+develop@gmail.com>
2021-06-10 12:37:01 +0900

    [feat] Add in-memory driver to make CLI work

--- reviewlogic/reviewlogic/drivers/review_driver.py
+++ reviewlogic/reviewlogic/drivers/review_driver.py
@@ -2,7 +2,8 @@ import abc
 from typing import TypedDict
 
 
-class ReviewEntity(TypedDict):
+class ReviewEntity(TypedDict, total=False):
+    id: int
     user_id: int
     proposal_id: int
     score: str
@@ -13,3 +14,13 @@ class ReviewDriver(abc.ABC):
     @abc.abstractmethod
     def save(self, entity):
         raise NotImplementedError
+
+
+class InMemoryReviewDriver(ReviewDriver):
+    review_entities: list[ReviewEntity] = []
+
+    def save(self, entity: ReviewEntity) -> int:
+        if "id" not in entity:
+            entity["id"] = len(self.review_entities) + 1
+        self.review_entities.append(entity)
+        return entity["id"]
